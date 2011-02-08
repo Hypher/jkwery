a679f61f7b84b5925732cf5926e3cc9f648c9b6c
Tj Holowaychuk <tj@vision-media.ca>
2011-02-08 03:54:57 -0800

    reducing

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var jsdom = require('jsdom')
  * jsdom options.
  */
 
-var options = {
+var jsdomOptions = {
   features: {
       FetchExternalResources: false
     , ProcessExternalResources: false
@@ -26,10 +26,45 @@ var stdin = process.openStdin()
   , buf = '';
 
 stdin.setEncoding('utf8');
-stdin.on('data', function(chunk){
-  buf += chunk;
-}).on('end', function(){
-  var window = jsdom.jsdom(buf, null, options).createWindow();
-  var $ = jquery.create(window);
-  console.log($('html').html());
-});
+stdin
+  .on('data', function(chunk){ buf += chunk; })
+  .on('end', parseArguments);
+
+/**
+ * Parse argv.
+ */
+
+function parseArguments() {
+  var arg
+    , args = process.argv.slice(2)
+    , options = {};
+
+  while (args.length) {
+    arg = args.shift();
+    switch (arg) {
+      default:
+        options.selectors = options.selectors || [];
+        options.selectors.push(arg);
+    }
+  }
+
+  parse(buf, options);
+}
+
+/**
+ * Parse and apply jQuery.
+ */
+
+function parse(html, options) {
+  var window = jsdom.jsdom(html, null, jsdomOptions).createWindow()
+    , $ = jquery.create(window)
+    , selectors = options.selectors || ['*']
+    , ctx;
+
+  while (selectors.length) {
+    ctx = $(selectors.shift());
+  }
+
+  console.log(ctx.html());  
+}
+
